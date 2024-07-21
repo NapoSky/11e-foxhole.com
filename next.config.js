a6f1c55e63f50a0833b710ed04b3f39e54926a1b
Alexandre Mompach <amompach@free.fr>
2024-07-21 22:14:32 +0200

    chore: add export config to nextJS

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
 const nextConfig = {
+  output: "export",
   webpack: config => {
     const oneOfRule = config.module.rules.find(rule => rule.oneOf);
 
@@ -24,15 +25,7 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: false,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },{
-        protocol: 'https',
-        hostname: 'source.unsplash.com',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
