3fad371ccd386094914e9d35e6511b9a907f0c74
Alexandre Mompach <31205250+NapoSky@users.noreply.github.com>
2024-07-24 06:37:14 +0200

    feat: add robots and sitemap, lint the whole code (#13)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,21 @@
-import dynamic from 'next/dynamic';
-import {FC, memo} from 'react';
+import dynamic from "next/dynamic";
+import { FC, memo } from "react";
 
-import Page from '../components/Layout/Page';
-import Footer from '../components/Sections/Footer';
-import Description from '../components/Sections/Description';
-import Operations from '../components/Sections/Operations';
-import Activities from '../components/Sections/Activities';
+import Page from "../components/Layout/Page";
+import Footer from "../components/Sections/Footer";
+import Description from "../components/Sections/Description";
+import Operations from "../components/Sections/Operations";
+import Activities from "../components/Sections/Activities";
 // import OfficerSection from '../components/Sections/Officers';
-import {homePageMeta} from '../data/data';
+import { homePageMeta } from "../data/data";
 
 // eslint-disable-next-line react-memo/require-memo
-const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
+const Header = dynamic(() => import("../components/Sections/Header"), {
+  ssr: false,
+});
 
 const Home: FC = memo(() => {
-  const {title, description} = homePageMeta;
+  const { title, description } = homePageMeta;
   return (
     <Page description={description} title={title}>
       <Header />
