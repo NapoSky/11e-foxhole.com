2619b32129d3c84bc51c9e08083e6778ead57c81
Alexandre Mompach <amompach@free.fr>
2024-07-30 02:24:35 +0200

    refactor: enhance how images are loaded to optimise SEO

--- src/components/Sections/Operations.tsx
+++ src/components/Sections/Operations.tsx
@@ -27,6 +27,7 @@ const Operations: FC = memo(() => {
         <div className=" w-full columns-2">
           {operationItems.map((item, index) => {
             const { title, image } = item;
+            const baseUrl = typeof image === 'string' ? image.replace(/\.webp$/, '') : '';
             return (
               <div className="pb-6" key={`${title}-${index}`}>
                 <div
@@ -38,7 +39,13 @@ const Operations: FC = memo(() => {
                     alt={title}
                     placeholder="blur"
                     src={image}
-                    sizes="(max-width: 768px) 100vw, 50vw"
+                    srcSet={`
+                      ${baseUrl}-320.webp 320w,
+                      ${baseUrl}-640.webp 640w,
+                      ${baseUrl}-1280.webp 1280w,
+                      ${baseUrl}-1920.webp 1920w
+                    `}
+                    sizes="(max-width: 768px) 100vw, 50vw, 33vw"
                     quality={75}
                   />
                   <ItemOverlay item={item} />
