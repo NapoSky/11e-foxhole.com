8e2ebc56cd7b6755c1b30bd89923069db3508359
Alexandre Mompach <31205250+NapoSky@users.noreply.github.com>
2024-08-11 16:37:32 +0200

    chore: modify how to handle meta tags to have each tag correctly defined per locale

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,46 +1,77 @@
+// src/components/Layout/Page.tsx
 import { NextPage } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { memo, PropsWithChildren } from "react";
-
+import { memo, PropsWithChildren, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { HomepageMeta } from "../../data/dataDef";
+import GoogleAnalytics from "../../components/GoogleAnalytics";
+import Header from "../../components/Sections/Header";
+import Schema from "../../components/Schema";
+import Footer from "../../components/Sections/Footer";
+import Description from "../../components/Sections/Description";
+import Operations from "../../components/Sections/Operations";
+import Activities from "../../components/Sections/Activities";
 
-const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(
-  ({ children, title, description }) => {
-    const { asPath: pathname } = useRouter();
+type PageProps = PropsWithChildren<HomepageMeta & { schemaData: any; fullUrl: string }>;
+
+const Page: NextPage<PageProps> = memo(
+  ({ children, title, description, schemaData, fullUrl }) => {
+    const { t, i18n } = useTranslation();
+
+    // États pour les valeurs traduites des balises meta
+    const [translatedTitle, setTranslatedTitle] = useState(title);
+    const [translatedDescription, setTranslatedDescription] = useState(description);
+
+    useEffect(() => {
+      // Forcer la langue à 'fr' lors du premier rendu
+      // Trick pour forcer la langue FR sur la page racine '/'
+      if (i18n.language !== 'fr') {
+        i18n.changeLanguage('fr').then(() => {
+          // Met à jour les traductions une fois la langue changée pour les pages [locale]
+          setTranslatedTitle(t(title));
+          setTranslatedDescription(t(description));
+        });
+      } else {
+        // Si la langue est déjà 'fr', pas besoin de changer, juste définir les traductions
+        setTranslatedTitle(t(title));
+        setTranslatedDescription(t(description));
+      }
+    }, [i18n, t, title, description]);
 
     return (
       <>
         <Head>
-          <title>{title}</title>
-          <meta content={description} name="description" />
+          <title>{translatedTitle}</title>
+          <meta name="description" content={translatedDescription} />
 
-          {/* several domains list the same content, make sure google knows we mean this one. */}
-          <link
-            href={`https://11e-foxhole.com${pathname}`}
-            key="canonical"
-            rel="canonical"
-          />
-
+          <link rel="canonical" href={fullUrl} />
           <link href="/favicon.ico" rel="icon" sizes="any" />
           <link href="/site.webmanifest" rel="manifest" />
 
-          {/* Open Graph : https://ogp.me/ */}
-          <meta content={title} property="og:title" />
-          <meta content={description} property="og:description" />
-          <meta
-            content={`https://11e-foxhole.com${pathname}`}
-            property="og:url"
-          />
+          <meta property="og:title" content={translatedTitle} />
+          <meta property="og:description" content={translatedDescription} />
+          <meta property="og:url" content={fullUrl} />
 
-          {/* Twitter: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
-          <meta content={title} name="twitter:title" />
-          <meta content={description} name="twitter:description" />
+          <meta name="twitter:title" content={translatedTitle} />
+          <meta name="twitter:description" content={translatedDescription} />
+
+          <script type="application/ld+json">
+            {JSON.stringify(schemaData)}
+          </script>
         </Head>
+        
+        <GoogleAnalytics />
+        <Header />
+        <Schema schema={schemaData} />
+        <Description />
+        <Activities />
+        <Operations />
+        <Footer />
+
         {children}
       </>
     );
-  },
+  }
 );
 
 Page.displayName = "Page";
